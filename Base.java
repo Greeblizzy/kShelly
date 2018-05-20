509f3cd6c94c9f68ffbbc4cffdc16f7993bcc948
Greeblizzy <[lisazheng123@gmail.com]>
2018-05-20 02:14:31 -0400

    added getters and setter methods, and completed the constructor

--- Base.java
+++ Base.java
@@ -1,12 +1,27 @@
 public abstract class Base {
     String  name;
     double size;
-    public Base()
+    public Base(String name)
     {
-
-
+        this.name = name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setSize(double size)
+    {
+        this.size = size;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public double getSize()
+    {
+        return size;
     }
     public boolean isFile(){
         return false;
-    }
+    }   
 }
